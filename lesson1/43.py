6dfb3770729b7e56c015ef6bd361d98436aad058
LG <lg2669@gmail.com>
2016-07-10 11:12:15 -0700

    saveme

---
+++ lesson1/43.py
@@ -0,0 +1,7 @@
+s = 'anystring'
+
+print s[3] + ' ' + s[1+1+1]
+print s[0] + ' ' + (s+s)[0]
+print s[0]+s[1] + ' ' + s[0+1]
+print s[1] + ' ' + (s+'ity')[1] #1 character your screwed
+print s[-1] + ' ' + (s+s)[-1]
