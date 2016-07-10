3b382eb6b252279d9fd7cf998a5c65a752c867d6
LG <lg2669@gmail.com>
2016-07-10 10:49:39 -0700

    another save

---
+++ lesson1/31.py
@@ -0,0 +1,9 @@
+minutes=minutes+1 #wtf minutes
+seconds = minutes*60
+
+print seconds
+
+#Traceback (most recent call last):
+#      File "31.py", line 1, in <module>
+#          minutes=minutes+1
+#          NameError: name 'minutes' is not defined
---
+++ lesson1/33.py
@@ -0,0 +1,4 @@
+age = 10
+days_in_year = 365
+
+print age * days_in_year
