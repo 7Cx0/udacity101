d458451ed37809c601371fc68733523432f37415
LG <lg2669@gmail.com>
2016-07-14 16:35:17 -0700

    save

---
+++ lesson2/hw/05.py
@@ -0,0 +1,7 @@
+def median(a,b,c):
+    return (a+b+c)/3
+
+print median(1,2,3)
+
+print median(9,3,6)
+print median(7,8,7)
---
+++ lesson2/hw/07.py
@@ -0,0 +1,7 @@
+def countdown(num):
+    while num > 0:
+        print num
+        num-=1
+    print "Blastoff"
+
+countdown(5)
